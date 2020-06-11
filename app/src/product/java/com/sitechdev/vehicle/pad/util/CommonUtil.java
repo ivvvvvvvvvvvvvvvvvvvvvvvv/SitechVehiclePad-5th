9ca029bb92ca6053b1c33e7e2ac4d56a78e6da66
shaozhi <tiger_shao@xtev.cn>
2020-06-11 14:43:25 +0800

    [refactor] 删除无效引用

--- app/src/product/java/com/sitechdev/vehicle/pad/util/CommonUtil.java
+++ app/src/product/java/com/sitechdev/vehicle/pad/util/CommonUtil.java
@@ -1,35 +1,8 @@
 package com.sitechdev.vehicle.pad.util;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.content.res.Resources;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.ScaleAnimation;
 
-import com.blankj.utilcode.util.AdaptScreenUtils;
-import com.blankj.utilcode.util.ScreenUtils;
 import com.blankj.utilcode.util.ToastUtils;
-import com.lzy.okgo.OkGo;
-import com.sitechdev.vehicle.lib.util.SitechDevLog;
-import com.sitechdev.vehicle.pad.app.AppConst;
-import com.sitechdev.vehicle.pad.manager.UserManager;
-import com.sitechdev.vehicle.pad.module.login.LoginActivity;
-import com.sitechdev.vehicle.pad.view.CommonToast;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.List;
 
 /**
  * 项目名称：SitechVehiclePad
