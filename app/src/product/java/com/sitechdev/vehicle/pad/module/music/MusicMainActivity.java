4796d134b60c0489c0a7fea435217ba73f2bd8c6
cold <cold_han@xtev.cn>
2020-06-12 14:07:33 +0800

    [fix] 去除无用代码

--- app/src/product/java/com/sitechdev/vehicle/pad/module/music/MusicMainActivity.java
+++ app/src/product/java/com/sitechdev/vehicle/pad/module/music/MusicMainActivity.java
@@ -1,12 +1,10 @@
 package com.sitechdev.vehicle.pad.module.music;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.flyco.tablayout.SlidingTabLayout;
@@ -16,10 +14,7 @@ import com.sitechdev.vehicle.pad.app.BaseActivity;
 import com.sitechdev.vehicle.pad.module.music.adapter.MusicPagerAdapter;
 import com.sitechdev.vehicle.pad.module.music.fragment.LocalMusicFragment;
 import com.sitechdev.vehicle.pad.module.music.fragment.OtherMusicFragment;
-import com.sitechdev.vehicle.pad.module.music.service.MusicInfo;
 import com.sitechdev.vehicle.pad.router.RouterConstants;
-import com.sitechdev.vehicle.pad.view.CommonToast;
-import com.sitechdev.vehicle.pad.view.ScrollTextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,21 +22,10 @@ import java.util.List;
 @Route(path = RouterConstants.FRAGMENT_LOCAL_MUSIC)
 public class MusicMainActivity extends BaseActivity {
 
-    private Context context;
-
     private SlidingTabLayout vTab;
     private ViewPager vPager;
     private String[] mTitles = {"USB", "蓝牙"};
-    private List<Fragment> mFragments;
-    private MusicPagerAdapter mAdapter;
-    private PlayHolder playHolder;
     private int index = 0;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        context = this;
-    }
 
     @Override
     protected void onNewIntent(Intent intent) {
@@ -69,10 +53,10 @@ public class MusicMainActivity extends BaseActivity {
         super.initView(savedInstanceState);
         vTab = findViewById(R.id.music_main_tablayout);
         vPager = findViewById(R.id.music_main_viewpager);
-        mFragments = new ArrayList<>();
+        List<Fragment> mFragments = new ArrayList<>();
         mFragments.add(LocalMusicFragment.newInstance());
         mFragments.add(OtherMusicFragment.newInstance());
-        mAdapter = new MusicPagerAdapter(getSupportFragmentManager(), mFragments);
+        MusicPagerAdapter mAdapter = new MusicPagerAdapter(getSupportFragmentManager(), mFragments);
         vPager.setAdapter(mAdapter);
         vTab.setViewPager(vPager, mTitles);
         vTab.setOnTabSelectListener(new OnTabSelectListener() {
@@ -97,11 +81,6 @@ public class MusicMainActivity extends BaseActivity {
 
             @Override
             public void onPageSelected(int position) {
-                if (position == 1 && null != playHolder) {
-//                    playHolder.show();
-                } else {
-                    playHolder.hide();
-                }
                 int count = vTab.getTabCount();
                 for (int i = 0; i < count; i++) {
                     vTab.getTitleView(i).setTextSize(24);
@@ -114,17 +93,7 @@ public class MusicMainActivity extends BaseActivity {
 
             }
         });
-        playHolder = new PlayHolder(findViewById(R.id.play_detail_status));
-        MusicManager.getInstance().addMusicChangeListener(playHolder);
-        MusicManager.getInstance().addMusicListUpdateListener(playHolder);
         analyzeIntent(getIntent());
-    }
-
-    @Override
-    protected void onDestroy() {
-        MusicManager.getInstance().removeMusicChangeListener(playHolder);
-        MusicManager.getInstance().removeMusicListUpdateListener(playHolder);
-        super.onDestroy();
     }
 
     @Override
@@ -148,106 +117,4 @@ public class MusicMainActivity extends BaseActivity {
         }
     }
 
-    private static class PlayHolder implements View.OnClickListener,
-            MusicManager.OnMusicChangeListener,
-            MusicManager.OnMusicListUpdateListener {
-
-        private View holderView;
-
-        private ImageView btn_pre;
-        private ImageView btn_next;
-        private ImageView btn_pause_play;
-        private ScrollTextView tv_bottom_title;
-
-        public PlayHolder(View view) {
-            this.holderView = view;
-            findView();
-        }
-
-        private void findView() {
-            btn_pre = holderView.findViewById(R.id.btn_pre);
-            btn_next = holderView.findViewById(R.id.btn_next);
-            btn_pause_play = holderView.findViewById(R.id.btn_pause_play);
-            holderView.findViewById(R.id.btn_pop_list).setVisibility(View.GONE);
-            tv_bottom_title = holderView.findViewById(R.id.tv_bottom_title);
-            btn_pre.setOnClickListener(this);
-            btn_next.setOnClickListener(this);
-            btn_pause_play.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.btn_pre:
-                    MusicManager.getInstance().pre(new MusicManager.CallBack<String>() {
-                        @Override
-                        public void onCallBack(int code, String s) {
-                            if (code != 0) {
-                                CommonToast.showToast(s);
-                            }
-                        }
-                    });
-                    break;
-                case R.id.btn_next:
-                    MusicManager.getInstance().next(new MusicManager.CallBack<String>() {
-                        @Override
-                        public void onCallBack(int code, String s) {
-                            if (code != 0) {
-                                CommonToast.showToast(s);
-                            }
-                        }
-                    });
-                    break;
-                case R.id.btn_pause_play:
-                    MusicManager.getInstance().toggle(new MusicManager.CallBack<String>() {
-                        @Override
-                        public void onCallBack(int code, String s) {
-                            if (code != 0) {
-                                CommonToast.showToast(s);
-                            }
-                        }
-                    });
-                    break;
-            }
-        }
-
-        public void show() {
-            if (null != holderView) {
-                holderView.setVisibility(View.VISIBLE);
-            }
-        }
-
-        public void hide() {
-            if (null != holderView) {
-                holderView.setVisibility(View.GONE);
-            }
-        }
-
-        @Override
-        public void onMusciChange(MusicInfo current, int status) {
-            if (null != current) {
-                tv_bottom_title.setText("");
-                tv_bottom_title.append(current.musicName);
-                tv_bottom_title.append(" - ");
-                tv_bottom_title.append(current.artist);
-                switch (status) {
-                    case MusicManager.OnMusicChangeListener.PAUSE:
-                        btn_pause_play.setActivated(false);
-                        break;
-                    case MusicManager.OnMusicChangeListener.RESUME:
-                        btn_pause_play.setActivated(true);
-                        break;
-                }
-            } else {
-                tv_bottom_title.setText("");
-                btn_pause_play.setActivated(false);
-            }
-        }
-
-        @Override
-        public void onMusicListUpdate(List<MusicInfo> infos, MusicInfo info, int status,
-                                      int postion) {
-            //TODO
-        }
-    }
 }
