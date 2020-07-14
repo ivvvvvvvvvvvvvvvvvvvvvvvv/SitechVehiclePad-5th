f284774f3a4ed51efa74b545b7bc376c94d6e89b
shaozhi <tiger_shao@xtev.cn>
2020-07-14 16:22:57 +0800

    【refactor】定位间隔延长至10秒

--- app/src/product/java/com/sitechdev/vehicle/pad/module/location/LocationUtil.java
+++ app/src/product/java/com/sitechdev/vehicle/pad/module/location/LocationUtil.java
@@ -39,7 +39,7 @@ public class LocationUtil {
     /**
      * 定位次数
      */
-    private int locCount = 0;
+    private int locCount = 0, intervalSecond = 10;
 
     private volatile static boolean isInited = false;
 
@@ -177,7 +177,7 @@ public class LocationUtil {
         mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
         mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
         mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        mOption.setInterval(5 * 1000);//可选，设置定位间隔。默认为2秒
+        mOption.setInterval(intervalSecond * 1000);//可选，设置定位间隔。默认为2秒
         mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
         mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
         mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
